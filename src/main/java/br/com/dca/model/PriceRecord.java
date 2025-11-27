b6ccb3862103c174dc1f5fa90dfd102cee09a244
Guilherme Nunes <guilherme.augd@gmail.com>
2025-11-27 15:46:43 -0300

    add class constructor and getters

--- src/main/java/br/com/dca/model/PriceRecord.java
+++ src/main/java/br/com/dca/model/PriceRecord.java
@@ -1,4 +1,20 @@
 package br.com.dca.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class PriceRecord {
+    private int assetId;
+    private LocalDate date;
+    private BigDecimal closePrice;
+
+    public PriceRecord(int assetId, LocalDate date, BigDecimal closePrice) {
+        this.assetId = assetId;
+        this.date = date;
+        this.closePrice = closePrice;
+    }
+
+    public int getAssetId() { return assetId; }
+    public LocalDate getDate() { return date; }
+    public BigDecimal getClosePrice() { return closePrice; }
 }
