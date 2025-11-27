31b48bf8996c9e9dbf6bb21640054d53d42384eb
Guilherme Nunes <guilherme.augd@gmail.com>
2025-11-27 16:53:32 -0300

    implement csv parser logic

--- src/main/java/br/com/dca/util/CsvParser.java
+++ src/main/java/br/com/dca/util/CsvParser.java
@@ -1,4 +1,44 @@
 package br.com.dca.util;
 
+import br.com.dca.model.PriceRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CsvParser {
+    public List<PriceRecord> parse(String filePath, int assetId) {
+        List<PriceRecord> records = new ArrayList<>();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+
+                if (values.length < 2) continue;
+
+                try {
+                    String rawDate = values[0].replace("\"", "");
+                    String rawPrice = values[1].replace("\"", "");
+
+                    LocalDate date = LocalDate.parse(rawDate, formatter);
+                    rawPrice = rawPrice.replace(".", "").replace(",", ".");
+
+                    BigDecimal price = new BigDecimal(rawPrice);
+                    records.add(new PriceRecord(assetId, date, price));
+                } catch (Exception e) {
+                    System.err.println("Skipping invalid row: " + line + " -> " + e.getMessage());
+                }
+            }
+        }
+    }
+
 }
