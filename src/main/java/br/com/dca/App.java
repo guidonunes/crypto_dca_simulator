1fa5853d5cf8c4393c4a91e442f96f84ad89751c
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-01 06:34:01 -0300

    implement logic to process data

--- src/main/java/br/com/dca/App.java
+++ src/main/java/br/com/dca/App.java
@@ -1,4 +1,5 @@
 package br.com.dca;
+import br.com.dca.dao.AssetPriceDAO;
 import br.com.dca.factory.ConnectionFactory;
 import br.com.dca.model.PriceRecord;
 import br.com.dca.util.CsvParser;
@@ -17,25 +18,57 @@ public class App {
 //            System.err.println(e.getMessage());
 //        }
         //CsvParser test
-        CsvParser csvParser = new CsvParser();
-        String btcPath = "src/main/resources/btc_brl_history.csv";
+//        CsvParser csvParser = new CsvParser();
+//        String btcPath = "src/main/resources/btc_brl_history.csv";
+//
+//        System.out.println("---1. Testing the CSV Parser");
+//        List<PriceRecord> btcData = csvParser.parse(btcPath, 1);
+//
+//        if (btcData.isEmpty()) {
+//            System.out.println("---1. No data found");
+//            return;
+//        }
+//
+//        System.out.println("Successfuly parsed " + btcData.size() + " data");
+//
+//        System.out.println("\n First 5 Records --- Verify if it is right");
+//        for (int i = 0; i < Math.min(5, btcData.size()); i++) {
+//            PriceRecord record = btcData.get(i);
+//
+//            System.out.println("Date: " + record.getDate() + " | Price: R$ " + record.getClosePrice());
+//        }
 
-        System.out.println("---1. Testing the CSV Parser");
-        List<PriceRecord> btcData = csvParser.parse(btcPath, 1);
+        //DAO Test
+        long startTime = System.currentTimeMillis();
 
-        if (btcData.isEmpty()) {
-            System.out.println("---1. No data found");
-            return;
+        try {
+            CsvParser csvParser = new CsvParser();
+            AssetPriceDAO assetPriceDAO = new AssetPriceDAO();
+
+            System.out.println("-- Processing Bitcoin Data --");
+            List<PriceRecord> btcData = csvParser.parse("src/main/resources/btc_brl_history.csv", 1);
+
+            if (!btcData.isEmpty()) {
+                assetPriceDAO.bulkInsertPrices(btcData);
+            } else {
+                System.err.println("No Bitcoin data found!");
+            }
+
+            System.out.println("\n-- Processing Ethereum Data");
+            List<PriceRecord> ethData = csvParser.parse("src/main/resources/eth_brl_history.csv", 2);
+
+            if (!ethData.isEmpty()) {
+                assetPriceDAO.bulkInsertPrices(ethData);
+            } else {
+                System.err.println("No Ethereum data found!");
+
+            }
+        } catch (SQLException ex) {
+            System.err.println("Database error: " + ex.getMessage());
+            ex.printStackTrace();
         }
-
-        System.out.println("Successfuly parsed " + btcData.size() + " data");
-
-        System.out.println("\n First 5 Records --- Verify if it is right");
-        for (int i = 0; i < Math.min(5, btcData.size()); i++) {
-            PriceRecord record = btcData.get(i);
-
-            System.out.println("Date: " + record.getDate() + " | Price: R$ " + record.getClosePrice());
-        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime - startTime) + " ms");
 
     }
 }
