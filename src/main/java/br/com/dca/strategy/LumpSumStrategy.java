22e02cbe6966214949ab77af9750e40edfeb0227
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-02 18:42:05 -0300

    implement lump sum logic

--- src/main/java/br/com/dca/strategy/LumpSumStrategy.java
+++ src/main/java/br/com/dca/strategy/LumpSumStrategy.java
@@ -3,10 +3,45 @@ package br.com.dca.strategy;
 import br.com.dca.model.PriceRecord;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 
 public class LumpSumStrategy implements InvestmentStrategy {
+    @Override
     public BigDecimal calculate (List<PriceRecord> prices, BigDecimal amount) {
-        return null;
+        if(prices == null || prices.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
+        prices.sort(Comparator.comparing(PriceRecord::getDate));
+
+        System.out.println("\n -- Simulating Lump Sum Strategy --");
+
+        PriceRecord firstRecord = prices.get(0);
+        BigDecimal initialPrice = firstRecord.getClosePrice();
+
+        BigDecimal cryptoAccumulated = amount.divide(initialPrice, 8, RoundingMode.HALF_UP);
+        PriceRecord lastRecord = prices.get(prices.size()-1);
+        BigDecimal finalPrice = lastRecord.getClosePrice();
+
+        BigDecimal finalPortfolioValue = cryptoAccumulated.multiply(finalPrice);
+
+        System.out.println("Date of Buy:    " + firstRecord.getDate());
+        System.out.println("Initial Price:  R$ " + initialPrice);
+        System.out.println("Total Invested: R$ " + amount);
+        System.out.println("Final Value:    R$ " + finalPortfolioValue);
+
+        if(amount.compareTo(BigDecimal.ZERO) > 0) {
+            BigDecimal profit = finalPortfolioValue.subtract(amount);
+            BigDecimal gainRatio = profit.divide(finalPrice, 4, RoundingMode.HALF_UP);
+            BigDecimal percentGain = gainRatio.multiply(new BigDecimal("100"));
+
+            System.out.println("Profit/Loss: R$ " + profit);
+            System.out.println("Total Gain: " + percentGain + "%");
+        }
+
+        return finalPortfolioValue;
     }
 }
