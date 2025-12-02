8c8371b04d86f6a2b6e5f4654129aea0a5992039
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-01 22:09:10 -0300

    implement DCA logic complete

--- src/main/java/br/com/dca/strategy/DcaStrategy.java
+++ src/main/java/br/com/dca/strategy/DcaStrategy.java
@@ -3,6 +3,9 @@ package br.com.dca.strategy;
 import br.com.dca.model.PriceRecord;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 
 public class DcaStrategy implements InvestmentStrategy {
@@ -11,5 +14,34 @@ public class DcaStrategy implements InvestmentStrategy {
         if(prices == null || prices.isEmpty()) {
             return BigDecimal.ZERO;
         }
+        prices.sort(Comparator.comparing(PriceRecord::getDate);
+
+        BigDecimal totalCryptoAccumulated = BigDecimal.ZERO;
+        BigDecimal totalCashInvested =  BigDecimal.ZERO;
+
+        LocalDate nextBuyDate = prices.get(0).getDate();
+
+        System.out.println("\n -- Simulating DCA Strategy --");
+        for (PriceRecord record : prices) {
+            LocalDate currentDate = record.getDate();
+
+            if(!currentDate.isBefore(nextBuyDate)) {
+                BigDecimal cryptoBought = investmentAmount.divide(
+                        record.getClosePrice(),
+                        8,
+                        RoundingMode.HALF_UP
+                );
+
+                totalCryptoAccumulated = totalCryptoAccumulated.add(cryptoBought);
+                totalCashInvested = totalCashInvested.add(cryptoBought);
+
+                nextBuyDate = nextBuyDate.plusDays(30);
+            }
+        }
+
+        BigDecimal lastPrice = prices.get(prices.size()-1).getClosePrice();
+        BigDecimal finalPortfolioValue = totalCryptoAccumulated.multiply(lastPrice);
+
+        return finalPortfolioValue;
     }
 }
