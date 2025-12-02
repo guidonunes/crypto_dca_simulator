3725507c0f6e4cda4814ef4855214628aa7bba98
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-01 21:42:46 -0300

    implement calculate method and null case handling

--- src/main/java/br/com/dca/strategy/DcaStrategy.java
+++ src/main/java/br/com/dca/strategy/DcaStrategy.java
@@ -1,4 +1,15 @@
 package br.com.dca.strategy;
 
-public class DcaStrategy {
+import br.com.dca.model.PriceRecord;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class DcaStrategy implements InvestmentStrategy {
+    @Override
+    public BigDecimal calculate(List<PriceRecord> prices, BigDecimal investmentAmount) {
+        if(prices == null || prices.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+    }
 }
