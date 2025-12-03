afddce963e3dfc3f534ee737d599bf43874b9c01
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-03 15:25:45 -0300

    implement getStrategy logic for a dynamic user choice

--- src/main/java/br/com/dca/factory/StrategyFactory.java
+++ src/main/java/br/com/dca/factory/StrategyFactory.java
@@ -1,7 +1,21 @@
 package br.com.dca.factory;
 
+import br.com.dca.strategy.DcaStrategy;
+import br.com.dca.strategy.InvestmentStrategy;
+import br.com.dca.strategy.LumpSumStrategy;
+
 public class StrategyFactory {
 
+    public static InvestmentStrategy getStrategy(String strategyType) {
+        if (strategyType == null) {
+            throw new IllegalArgumentException("Strategy type must not be null");
+        }
 
+        return switch (strategyType.toUpperCase()) {
+            case "DCA" -> new DcaStrategy();
+            case "LUMP" -> new LumpSumStrategy();
+            default -> throw new IllegalArgumentException("Strategy type " + strategyType + " not supported");
+        };
+    }
 
 }
