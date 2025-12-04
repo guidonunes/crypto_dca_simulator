38a6656297eb40e821e6a4a4f2f12f74143bac33
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-04 14:20:43 -0300

    add class to abstract the simulation result

---
+++ src/main/java/br/com/dca/model/SimulationResult.java
@@ -0,0 +1,11 @@
+package br.com.dca.model;
+
+import java.math.BigDecimal;
+
+public class SimulationResult {
+    private String strategyName;
+    private BigDecimal initialInvestment;
+    private BigDecimal finalValue;
+    private BigDecimal profit;
+    private BigDecimal percentGain;
+}
