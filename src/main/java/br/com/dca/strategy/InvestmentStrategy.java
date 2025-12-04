761996201d319131a8c05cfc05eb83e99e21f5d6
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-04 14:37:55 -0300

    update interface return type

--- src/main/java/br/com/dca/strategy/InvestmentStrategy.java
+++ src/main/java/br/com/dca/strategy/InvestmentStrategy.java
@@ -1,9 +1,11 @@
 package br.com.dca.strategy;
 
 import br.com.dca.model.PriceRecord;
+import br.com.dca.model.SimulationResult;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface InvestmentStrategy {
-    BigDecimal calculate (List<PriceRecord> prices, BigDecimal investmentAmount);
+    SimulationResult calculate (List<PriceRecord> prices, BigDecimal investmentAmount);
 }
