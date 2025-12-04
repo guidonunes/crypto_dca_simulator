839ebd697fa5d1e9d1e006b7cbf34c905656b37f
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-04 15:32:28 -0300

    refactor lump sum class to return simulation result

--- src/main/java/br/com/dca/strategy/LumpSumStrategy.java
+++ src/main/java/br/com/dca/strategy/LumpSumStrategy.java
@@ -1,47 +1,42 @@
 package br.com.dca.strategy;
 
 import br.com.dca.model.PriceRecord;
+import br.com.dca.model.SimulationResult;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 
 public class LumpSumStrategy implements InvestmentStrategy {
     @Override
-    public BigDecimal calculate (List<PriceRecord> prices, BigDecimal amount) {
+    public SimulationResult calculate (List<PriceRecord> prices, BigDecimal amount) {
         if(prices == null || prices.isEmpty()) {
-            return BigDecimal.ZERO;
+            return new SimulationResult("LUMP", BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO);
         }
 
         prices.sort(Comparator.comparing(PriceRecord::getDate));
 
-        System.out.println("\n -- Simulating Lump Sum Strategy --");
-
-        PriceRecord firstRecord = prices.get(0);
-        BigDecimal initialPrice = firstRecord.getClosePrice();
-
+        BigDecimal initialPrice = prices.get(0).getClosePrice();
         BigDecimal cryptoAccumulated = amount.divide(initialPrice, 8, RoundingMode.HALF_UP);
-        PriceRecord lastRecord = prices.get(prices.size()-1);
-        BigDecimal finalPrice = lastRecord.getClosePrice();
 
+        BigDecimal finalPrice = prices.get(prices.size()-1).getClosePrice();
         BigDecimal finalPortfolioValue = cryptoAccumulated.multiply(finalPrice);
 
-        System.out.println("Date of Buy:    " + firstRecord.getDate());
-        System.out.println("Initial Price:  R$ " + initialPrice);
-        System.out.println("Total Invested: R$ " + amount);
-        System.out.println("Final Value:    R$ " + finalPortfolioValue);
+        BigDecimal profit = finalPortfolioValue.subtract(amount);
+        BigDecimal percentGain = BigDecimal.ZERO;
 
         if(amount.compareTo(BigDecimal.ZERO) > 0) {
-            BigDecimal profit = finalPortfolioValue.subtract(amount);
             BigDecimal gainRatio = profit.divide(amount, 4, RoundingMode.HALF_UP);
-            BigDecimal percentGain = gainRatio.multiply(new BigDecimal("100"));
-
-            System.out.println("Profit/Loss: R$ " + profit);
-            System.out.println("Percent Gain: " + percentGain + "%");
+            percentGain = gainRatio.multiply(new BigDecimal("100"));
         }
 
-        return finalPortfolioValue;
+        return new SimulationResult(
+                "LUMP",
+                amount,
+                finalPortfolioValue,
+                profit,
+                percentGain
+        );
     }
 }
