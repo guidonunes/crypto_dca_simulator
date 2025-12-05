89473bc45734f9fbddeaf4b6b0cca0cdc57b5552
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-05 17:44:02 -0300

    update documentation

--- src/main/java/br/com/dca/App.java
+++ src/main/java/br/com/dca/App.java
@@ -18,232 +18,148 @@ import java.util.Scanner;
 
 
 /**
- * Database connection test
- *       try {
- *             Connection connection = ConnectionFactory.getConnection();
- *             System.out.println("Successfully connected.");
- *         } catch (SQLException e) {
- *             System.err.println(e.getMessage());
- *         }
- *         //CsvParser test
- *         CsvParser csvParser = new CsvParser();
- *         String btcPath = "src/main/resources/btc_brl_history.csv";
- *
- *         System.out.println("---1. Testing the CSV Parser");
- *         List<PriceRecord> btcData = csvParser.parse(btcPath, 1);
- *
- *         if (btcData.isEmpty()) {
- *          System.out.println("---1. No data found");
- *             return;
- *         }
- *
- *         System.out.println("Successfuly parsed " + btcData.size() + " data");
- *
- *         System.out.println("\n First 5 Records --- Verify if it is right");
- *         for (int i = 0; i < Math.min(5, btcData.size()); i++) {
- *             PriceRecord record = btcData.get(i);
- *
- *             System.out.println("Date: " + record.getDate() + " | Price: R$ " + record.getClosePrice());
- *         }
- *
- * // Inserting CSV Data into the DB
- * Bitcoin :
- *            if (!btcData.isEmpty()) {
- *                 assetPriceDAO.bulkInsertPrices(btcData);
- *             } else {
- *                 System.err.println("No Bitcoin data found!");
- *             }
- * Ethereum :
- *
- *
- *             if (!ethData.isEmpty()) {
- *                 assetPriceDAO.bulkInsertPrices(ethData);
- *             } else {
- *                 System.err.println("No Ethereum data found!");
- *
- *             }
- *
- *  // DAO Test Hard coded
- *         long startTime = System.currentTimeMillis();
- *
- *         try {
- *             CsvParser csvParser = new CsvParser();
- *             AssetPriceDAO assetPriceDAO = new AssetPriceDAO();
- *
- *             System.out.println("-- Processing Bitcoin Data --");
- *             List<PriceRecord> btcData = csvParser.parse("src/main/resources/btc_brl_history.csv", 1);
- *
- *             if (!btcData.isEmpty()) {
- *                 DcaStrategy dca = new DcaStrategy();
- *                 LumpSumStrategy lumpSum = new LumpSumStrategy();
- *
- *                 // Simulate buying R$ 100.00 of Bitcoin every 30 days
- *                 dca.calculate(btcData, new BigDecimal("100") );
- *                 lumpSum.calculate(btcData, new BigDecimal("7200"));
- *             }
- *
- *             System.out.println("\n-- Processing Ethereum Data");
- *             List<PriceRecord> ethData = csvParser.parse("src/main/resources/eth_brl_history.csv", 2);
- *
- *             if (!ethData.isEmpty()) {
- *                 DcaStrategy dca = new DcaStrategy();
- *                 LumpSumStrategy lumpSum = new LumpSumStrategy();
- *
- *                 // Simulate buying R$ 100.00 of Bitcoin every 30 days
- *                 dca.calculate(ethData, new BigDecimal("100.00"));
- *                 lumpSum.calculate(ethData, new BigDecimal("7200"));
- *
- *             }
- *         } catch (SQLException ex) {
- *             System.err.println("Database error: " + ex.getMessage());
- *             ex.printStackTrace();
- *         }
- *         long endTime = System.currentTimeMillis();
- *         System.out.println("Total execution time: " + (endTime - startTime) + " ms");
- *
+     // Insert data
+
+     CsvParser csvParser = new CsvParser();
+     AssetPriceDAO assetPriceDAO = new AssetPriceDAO();
+     String assetPath = "src/main/resources/nasd11_brl_history.csv";
+     List<PriceRecord> assetData = csvParser.parse(assetPath, 8);
+
+     try {
+     if (!assetData.isEmpty()) {
+     assetPriceDAO.bulkInsertPrices(assetData);
+     } else {
+     System.err.println("No data found!");
+     }
+     } catch (Exception e) {
+     System.out.println(e.getMessage());
+     }
+
  *
  */
 
 public class App {
     public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        CsvParser parser = new CsvParser();
 
-        CsvParser csvParser = new CsvParser();
-        AssetPriceDAO assetPriceDAO = new AssetPriceDAO();
-        String assetPath = "src/main/resources/nasd11_brl_history.csv";
-        List<PriceRecord> assetData = csvParser.parse(assetPath, 8);
+        System.out.println(">> WELCOME TO DCA SIMULATOR <<");
+        System.out.println("Simulate strategy using historical data from 2020 to 2025");
 
-         try {
-              if (!assetData.isEmpty()) {
-                  assetPriceDAO.bulkInsertPrices(assetData);
-              } else {
-                  System.err.println("No data found!");
-              }
-          } catch (Exception e) {
-              System.out.println(e.getMessage());
-          }
+        int userChoice = -1;
+
+        try {
+            do {
+                System.out.println("""
+
+                        Choose an option:\
+
+                        1 - See available assets \
+
+                        2 - Choose an investment strategy to simulate \
+
+                        0 - Quit""");
+                System.out.print("Select an option: ");
+
+                if(scanner.hasNextInt()) {
+                    userChoice = scanner.nextInt();
+                    scanner.nextLine();
+                } else {
+                    scanner.next();
+                    userChoice = -1;
+                }
+
+                switch (userChoice) {
+                    case 1:
+                        System.out.println("\n---Available assets ---");
+                        System.out.println("1. Bitcoin (BTC)");
+                        System.out.println("2. Ethereum (ETH)");
+                        break;
+                    case 2:
+                        runSimulation(scanner, parser);
+                        break;
+                    case 0:
+                        System.out.println("Bye bye!");
+                        break;
+                    default:
+                        System.out.printf("Please select a valid option!\n");
+
+                }
 
 
+            } while (userChoice != 0);
+        } catch (Exception ex) {
+            System.err.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            scanner.close();
+        }
     }
-//        Scanner scanner = new Scanner(System.in);
-//        CsvParser parser = new CsvParser();
-//
-//        System.out.println(">> WELCOME TO DCA SIMULATOR <<");
-//        System.out.println("Simulate strategy using historical data from 2020 to 2025");
-//
-//        int userChoice = -1;
-//
-//        try {
-//            do {
-//                System.out.println("""
-//
-//                        Choose an option:\
-//
-//                        1 - See available assets \
-//
-//                        2 - Choose an investment strategy to simulate \
-//
-//                        0 - Quit""");
-//                System.out.print("Select an option: ");
-//
-//                if(scanner.hasNextInt()) {
-//                    userChoice = scanner.nextInt();
-//                    scanner.nextLine();
-//                } else {
-//                    scanner.next();
-//                    userChoice = -1;
-//                }
-//
-//                switch (userChoice) {
-//                    case 1:
-//                        System.out.println("\n---Available assets ---");
-//                        System.out.println("1. Bitcoin (BTC)");
-//                        System.out.println("2. Ethereum (ETH)");
-//                        break;
-//                    case 2:
-//                        runSimulation(scanner, parser);
-//                        break;
-//                    case 0:
-//                        System.out.println("Bye bye!");
-//                        break;
-//                    default:
-//                        System.out.printf("Please select a valid option!\n");
-//
-//                }
-//
-//
-//            } while (userChoice != 0);
-//        } catch (Exception ex) {
-//            System.err.println("Error: " + ex.getMessage());
-//            ex.printStackTrace();
-//        } finally {
-//            scanner.close();
-//        }
-//    }
-//
-//    private static void runSimulation(Scanner scanner, CsvParser parser) {
-//        System.out.println("\n-- New Simulation ---");
-//        // 1. Choose Strategy
-//        System.out.println("Enter Strategy (DCA or LUMP)");
-//        String strategyType = scanner.nextLine();
-//        // 2. Choose Asset
-//        System.out.println("Choose Asset:");
-//        System.out.println("1. Bitcoin (BTC)");
-//        System.out.println("2. Ethereum (ETH)");
-//        System.out.println("Enter choice: (e.g. 1)");
-//
-//        int assetChoice = -1;
-//        if(scanner.hasNextInt()) {
-//            assetChoice = scanner.nextInt();
-//            scanner.nextLine();
-//        } else {
-//            scanner.nextLine();
-//        }
-//        // 3. Call the helper method
-//        String filePath = getFilePathForAsset(assetChoice);
-//
-//        if(filePath == null) {
-//            System.out.println("Error: Asset not found!");
-//            return;
-//        }
-//
-//        // 4. Get Amount
-//        System.out.println("Enter Investment Amount (e.g. 100): ");
-//        String investmentAmount = scanner.nextLine();
-//        BigDecimal amount = new BigDecimal(investmentAmount);
-//
-//        // 5. Run the specific simulation
-//        InvestmentStrategy strategy = StrategyFactory.getStrategy(strategyType);
-//
-//        System.out.println("Processing data...");
-//        List<PriceRecord> data = parser.parse(filePath, assetChoice);
-//
-//        if(!data.isEmpty()) {
-//            SimulationResult result = strategy.calculate(data, amount);
-//            String assetName = (assetChoice == 1) ? "Bitcoin" : "Ethereum";
-//
-//            printReport(assetName, result);
-//            new SimulationDAO().save(result, assetName);
-//        }
-//    }
-//
-//
-//    private static void printReport(String assetName, SimulationResult result) {
-//        System.out.println("-------------------------------------");
-//        System.out.println(" Results for " + assetName);
-//        System.out.println("-------------------------------------");
-//        System.out.println("Strategy:       " + result.getStrategyName());
-//        System.out.println("Total Invested: R$ " + result.getInitialInvestment());
-//        System.out.println("Final Value:    R$ " + result.getFinalValue());
-//        System.out.println("Profit/Loss:    R$ " + result.getProfit());
-//        System.out.println("Percent Gain:   " + result.getPercentGain() + "%");
-//        System.out.println("-------------------------------------\n");
-//    }
-//
-//    private static String getFilePathForAsset(int assetChoice) {
-//        return switch (assetChoice) {
-//            case 1 -> "src/main/resources/btc_brl_history.csv";
-//            case 2 -> "src/main/resources/eth_brl_history.csv";
-//            default -> null;
-//        };
-//    }
+
+    private static void runSimulation(Scanner scanner, CsvParser parser) {
+        System.out.println("\n-- New Simulation ---");
+        // 1. Choose Strategy
+        System.out.println("Enter Strategy (DCA or LUMP)");
+        String strategyType = scanner.nextLine();
+        // 2. Choose Asset
+        System.out.println("Choose Asset:");
+        System.out.println("1. Bitcoin (BTC)");
+        System.out.println("2. Ethereum (ETH)");
+        System.out.println("Enter choice: (e.g. 1)");
+
+        int assetChoice = -1;
+        if(scanner.hasNextInt()) {
+            assetChoice = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            scanner.nextLine();
+        }
+        // 3. Call the helper method
+        String filePath = getFilePathForAsset(assetChoice);
+
+        if(filePath == null) {
+            System.out.println("Error: Asset not found!");
+            return;
+        }
+
+        // 4. Get Amount
+        System.out.println("Enter Investment Amount (e.g. 100): ");
+        String investmentAmount = scanner.nextLine();
+        BigDecimal amount = new BigDecimal(investmentAmount);
+
+        // 5. Run the specific simulation
+        InvestmentStrategy strategy = StrategyFactory.getStrategy(strategyType);
+
+        System.out.println("Processing data...");
+        List<PriceRecord> data = parser.parse(filePath, assetChoice);
+
+        if(!data.isEmpty()) {
+            SimulationResult result = strategy.calculate(data, amount);
+            String assetName = (assetChoice == 1) ? "Bitcoin" : "Ethereum";
+
+            printReport(assetName, result);
+            new SimulationDAO().save(result, assetName);
+        }
+    }
+
+
+    private static void printReport(String assetName, SimulationResult result) {
+        System.out.println("-------------------------------------");
+        System.out.println(" Results for " + assetName);
+        System.out.println("-------------------------------------");
+        System.out.println("Strategy:       " + result.getStrategyName());
+        System.out.println("Total Invested: R$ " + result.getInitialInvestment());
+        System.out.println("Final Value:    R$ " + result.getFinalValue());
+        System.out.println("Profit/Loss:    R$ " + result.getProfit());
+        System.out.println("Percent Gain:   " + result.getPercentGain() + "%");
+        System.out.println("-------------------------------------\n");
+    }
+
+    private static String getFilePathForAsset(int assetChoice) {
+        return switch (assetChoice) {
+            case 1 -> "src/main/resources/btc_brl_history.csv";
+            case 2 -> "src/main/resources/eth_brl_history.csv";
+            default -> null;
+        };
+    }
 }
