6981e065cb02ae0d9e1ea5425eb71156127f0b7f
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-11 20:52:03 -0300

    refactor the project into a spring boot app

---
+++ src/main/java/br/com/dcasimulator/DcasimulatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.dcasimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcasimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcasimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dcasimulator/DcasimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dcasimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcasimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
