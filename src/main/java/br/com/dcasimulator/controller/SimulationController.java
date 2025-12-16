07eb945e1a0c0083f3dd9a6e2345c555b9e3e360
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-16 20:47:41 -0300

    POST Endpoint: Run simulation

--- src/main/java/br/com/dcasimulator/controller/SimulationController.java
+++ src/main/java/br/com/dcasimulator/controller/SimulationController.java
@@ -1,9 +1,19 @@
 package br.com.dcasimulator.controller;
 
 
+import br.com.dcasimulator.entity.SimulationResult;
+import br.com.dcasimulator.model.PriceRecord;
+import br.com.dcasimulator.model.SimulationRequest;
 import br.com.dcasimulator.service.SimulationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/simulations")
@@ -13,4 +23,20 @@ public class SimulationController {
     public SimulationController(SimulationService simulationService) {
         this.simulationService = simulationService;
     }
+
+    @PostMapping
+    public SimulationResult runSimulation(@RequestBody SimulationRequest request) {
+        List<PriceRecord> fakePrices = new ArrayList<>();
+        fakePrices.add(new PriceRecord(1, LocalDate.now().minusMonths(6), new BigDecimal("50000")));
+        fakePrices.add(new PriceRecord(1, LocalDate.now().minusMonths(3), new BigDecimal("45000")));
+        fakePrices.add(new PriceRecord(1, LocalDate.now(), new BigDecimal("60000")));
+
+        if ("DCA".equalsIgnoreCase(request.strategy())) {
+            return simulationService.runDca(fakePrices, request.amount(), request.assetName());
+        } else {
+            return simulationService.runLumpSum(fakePrices, request.amount(), request.assetName());
+        }
+    }
+
+
 }
