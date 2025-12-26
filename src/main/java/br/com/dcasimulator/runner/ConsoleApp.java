8b8a0592c45ee729f0906f89a35fb8b7b49a865f
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-26 17:07:54 -0300

    implement save to database logic

--- src/main/java/br/com/dcasimulator/runner/ConsoleApp.java
+++ src/main/java/br/com/dcasimulator/runner/ConsoleApp.java
@@ -1,7 +1,9 @@
 package br.com.dcasimulator.runner;
 
+import br.com.dcasimulator.entity.Price;
 import br.com.dcasimulator.model.PriceRecord;
 import br.com.dcasimulator.entity.SimulationResult;
+import br.com.dcasimulator.repository.PriceRepository;
 import br.com.dcasimulator.service.CsvParser;
 import br.com.dcasimulator.service.SimulationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +20,18 @@ import java.util.Scanner;
 public class ConsoleApp implements CommandLineRunner {
 
     private final CsvParser csvParser;
+    private final PriceRepository priceRepository;
 
     @Autowired
-    public ConsoleApp(CsvParser csvParser) {
+    public ConsoleApp(CsvParser csvParser,  PriceRepository priceRepository) {
         this.csvParser = csvParser;
+        this.priceRepository = priceRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("--- 🟢 STARTING CSV IMPORT TEST ---");
 
-        // 1. Call the parser with your specific filename from 'resources'
-        // Make sure this matches the filename in your screenshot exactly!
         String fileName = "btc_brl_data.csv";
 
         List<PriceRecord> records = csvParser.loadDataFromCsv(fileName);
@@ -37,15 +39,26 @@ public class ConsoleApp implements CommandLineRunner {
 
         if (records.isEmpty()) {
             System.out.println("⚠️ No records found! Check the filename.");
-        } else {
-            System.out.println("✅ Successfully loaded " + records.size() + " records.");
-            System.out.println("--- First 5 Rows ---");
-            records.stream().limit(5).forEach(record -> {
-                System.out.println(record);
-            });
+            return;
         }
 
-        System.out.println("--- 🔴 END TEST ---");
+        System.out.println(" --> Parsed " + records.size() + " price records!");
+
+        List<Price> entitiesToSave = new ArrayList<>();
+
+        for(PriceRecord record : records) {
+            Price priceEntity = new Price(
+                    "BTC",
+                    record.getDate(),
+                    record.getClose()
+            );
+            entitiesToSave.add(priceEntity);
+        }
+
+        priceRepository.saveAll(entitiesToSave);
+
+        System.out.println("✅ SUCCESSFULLY SAVED " + entitiesToSave.size() + " ROWS TO DB!");
+        System.out.println("--- 🔴 IMPORT FINISHED ---");
     }
 
 
