c05ef1089b4732a662303bc0c40ef59175c9ca0c
Guilherme Nunes <guilherme.augd@gmail.com>
2026-01-19 22:10:25 -0300

    fix allowed origins

--- dca-simulator-backend/src/main/java/br/com/dcasimulator/config/CorsConfig.java
+++ dca-simulator-backend/src/main/java/br/com/dcasimulator/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000", "https://dca-sim.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
